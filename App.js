59eaa5e1c6ac2864e0c868096d9a3559b51f8c1c
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-07-29 22:36:23 +0530

    mongo db

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const app = express()
+dotenv.config({path:'./config/config.env'})
 app.get('/', (req,res)=>{
     res.send("Root Request")
 })
 
-app.listen(5000,(err)=>{
+app.listen(process.env.PORT,(err)=>{
+    console.log(process.env.PORT)
     if(err) throw err
     console.log(`Servr Running on port Number: ${5000}`)
 })
+
+/*
+    How to read Application configuration env variables;
+    1.install dotenv 
+    npm i dotenv 
+    2.import dotenv 
+    3.provide your configuration path.
+    dotenv.config({path:''})
+*/
